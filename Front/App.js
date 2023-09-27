d3b771a439715e73e70fc7a0e609f385da5b695a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-27 09:03:29 -0300

    creando

---
+++ Front/App.js
@@ -0,0 +1,55 @@
+/*import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;*/
+
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Modal, Text, TouchableOpacity, View } from 'react-native'; // Importa los componentes necesarios
+import LoginScreen from './FrontEnd/screens/Login';
+import Register from './FrontEnd/screens/SignUp';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setShowSuccessModal(true);
+  };
+
+  const closeModal = () => {
+    setShowSuccessModal(false);
+  };
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login">
+          {props => <LoginScreen {...props} onLoginSuccess={handleLoginSuccess} />}
+        </Stack.Screen>
+        <Stack.Screen name="SignUp" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
+
